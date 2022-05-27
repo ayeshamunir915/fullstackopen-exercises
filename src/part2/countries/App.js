a9a2bf9528e81582f409e390f592fa35a20e1c58
ayeshamunir915 <94769474+ayeshamunir915@users.noreply.github.com>
2022-05-27 10:39:58 -0500

    part2
    Signed-off-by: ayeshamunir915 <94769474+ayeshamunir915@users.noreply.github.com>

--- src/part2/countries/App.js
+++ src/part2/countries/App.js
@@ -1,61 +1,55 @@
-import { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
-const Country = (props) => {
-  if (props.allCountries.length === 0) {
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [search, setSearch] = useState('')
+
+  const Countries = (props) => {
+    if (countries.length > 10)
     return (
+    <div>
+      Too many matches, specify another filter
+    </div>
+    )
+    return (
+      props.countries.map(countries => {
+        return(
+          <Countries countries={countries} />
+        )
+      })
+    )
+  }
+
+  const CountryInformation = ({countries}) => {
+    return(
       <div>
-        Too many countries, specify another filter
+        <h2>{countries.name}</h2>
+        <p>Capital: {countries.capital}</p>
+        <p>Population: {countries.population}</p>
+        <h3>Languages</h3>
+        <div>
+          {countries.languages.map(language => <li key={language.name}>{language.name}</li>)}
+        </div>
+        <img src={countries.flag} alt="country flag" width="300vw"/>
       </div>
     )
   }
-}
-
-const Countries = (props) => {
-  const {name, capital, area}=props;
-  return (
-    <table>
-    <tr>
-      <td>name {name}</td>
-    </tr>
-    <tr>
-      <td>capital {capital}</td>
-    </tr>
-    <tr>
-      <td>area {area}</td>
-    </tr>
-  </table>
-  )
-}
-
-const Languages = (props) => {
-  const {languages}=props;
-  return (
-    <table>
-    <tr>
-      <td>languages {languages}</td>
-    </tr>
-  </table>
-  )
-}
-
-const App = () => {
-
-  const [newCountries, setNewCountries] = useState('')
 
   useEffect(() => {
     console.log('effect')
-    axios
-    .get('https://restcountries.com/')
-    .then(response => {
-      const notes = response.data
-      console.log(notes)
-    })
+    axios.get('https://restcountries.com/v3.1/all').then(response => {
+        console.log('promise fulfilled')
+        setCountries(response.data)
+      })
   }, [])
 
-  return (
+  return(
+    // {(countries.length >=10) ? <div>Too many matches, specify another filter</div> :
+    // <Countries countries={countries} />}
     <div>
-      Find countries <input />
+      find countries <input value={search} onChange={e => setSearch(e.target.value)} /><br></br>
+      <Countries countries={countries}/><button>Show</button>
     </div>
   )
 }
